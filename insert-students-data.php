d2d8e55488f0149e428ce64091d723661fddde24
AslamJM <88993157+AslamJM@users.noreply.github.com>
2022-06-27 15:11:25 +0530

    Get data in php

---
+++ insert-students-data.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$student_id= $_POST['student_id'];
+$firstname= $_POST['firstname'];
+$lastname= $_POST['lastname'];
+$dob= $_POST['dob'];
+$email= $_POST['email'];
+$department= $_POST['department'];
+
+echo $student_id;
+echo $firstname;
+echo $lastname;
+echo $dob;
+echo $email; 
+echo $department; 
+?>
