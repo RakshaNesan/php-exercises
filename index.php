1b8405914a3aa72e30429d2a8531389f81c0838e
AslamJM <88993157+AslamJM@users.noreply.github.com>
2022-06-27 14:22:54 +0530

    Add styles

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <title>Index</title>
+</head>
+<body>
+    <h1 style="text-align:center">Student Registration</h1><hr>
+ <div class="container" style="background-color:palevioletred">
+<form>
+  <div class="form-group" style="margin-bottom:20px;">
+    <label for="studentid">Student ID</label>
+    <input type="text" class="form-control" id="studentid" placeholder="Enter Student ID">
+</div>
+
+<div class="form-group"  style="margin-bottom:20px;">
+     <label for="firstname">First Name</label>
+    <input type="text" class="form-control" id="firstname" placeholder="Enter First Name">
+</div>
+
+    <div class="form-group"  style="margin-bottom:20px;">
+     <label for="lastname">Last Name</label>
+    <input type="text" class="form-control" id="lastname" placeholder="Enter Last Name">
+    </div>
+
+    <div class="form-group"  style="margin-bottom:20px;">
+     <label for="dateofbirth">Date Of Birth</label>
+    <input type="text" class="form-control" id="dateofbirth" placeholder="Enter Date Of Birth">
+    </div>
+
+    <div class="form-group"  style="margin-bottom:20px;">
+     <label for="email">Email</label>
+    <input type="text" class="form-control" id="email" placeholder="Enter Email">
+    </div>
+
+    <div class="form-group"  style="margin-bottom:20px;">
+     <label for="department">Department</label>
+    <input type="text" class="form-control" id="department" placeholder="Enter Department">
+  </div>
+   <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
